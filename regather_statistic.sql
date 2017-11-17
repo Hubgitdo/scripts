785f4530cc28abab03371501be1015192a7d50d4
Hubgitdo <33735319+Hubgitdo@users.noreply.github.com>
2017-11-17 10:08:39 -0700

    Update regather_statistic.sql

--- regather_statistic.sql
+++ regather_statistic.sql
@@ -1,4 +1,4 @@
 -- there are many ways/methods to gather statistics. DBA should determine what’s best for their own environment.
-EXEC DBMS_STATS.gather_schema_stats(‘VPXADMIN’, estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE, method_opt => 'for all columns size auto', cascade => TRUE);
+EXEC DBMS_STATS.gather_schema_stats('&schema', estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE, method_opt => 'for all columns size auto', cascade => TRUE);
 
-EXEC DBMS_STATS.gather_schema_stats(VPXADMIN, estimate_percent => 15, method_opt => 'for all columns size skewonly', cascade => TRUE);
+EXEC DBMS_STATS.gather_schema_stats('&schema', estimate_percent => 15, method_opt => 'for all columns size skewonly', cascade => TRUE);
